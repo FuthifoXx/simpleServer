9949ebbd3a4359b45c9edd51eb58a9850bf90a88
FuthifoXx <futhifoxmaseko@gmail.com>
2025-05-07 12:27:36 +0200

    authModel, contronller and route

---
+++ controllers/authController.js
@@ -0,0 +1,18 @@
+const User = require('../model/userModel');
+
+exports.signup = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user: newUser
+      }
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error
+    });
+  }
+};
