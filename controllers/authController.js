fee058f3d1662f90b9ccc4ededf0cbf4391398ad
FuthifoXx <futhifoxmaseko@gmail.com>
2025-05-08 11:31:50 +0200

    logging in Users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,12 @@
 const jwt = require('jsonwebtoken');
 const User = require('../model/userModel');
 
+const signToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+};
+
 exports.signup = async (req, res) => {
   try {
     const newUser = await User.create({
@@ -10,9 +16,7 @@ exports.signup = async (req, res) => {
       passwordConfirm: req.body.passwordConfirm
     });
 
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN
-    });
+    const token = signToken(newUser._id);
 
     res.status(201).json({
       status: 'success',
@@ -20,6 +24,40 @@ exports.signup = async (req, res) => {
       data: {
         user: newUser
       }
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error
+    });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //1) Check if user exists && password is correct
+    if (!email || !password) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'Please provide email and password'
+      });
+    }
+    //2) Check if user exists && password is correct
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'Incorrect email or password'
+      });
+    }
+    //3) If everything ok, send token to client
+    const token = signToken(user._id);
+    res.status(200).json({
+      status: 'success',
+      token
     });
   } catch (error) {
     res.status(404).json({
